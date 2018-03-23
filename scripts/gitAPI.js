72faf9e81f30e15a7225e35de1743c9351298a46
Stef van Dijk <stef@vandijkstef.nl>
2018-03-23 10:14:41 +0100

    temp code to rewrite

--- scripts/gitAPI.js
+++ scripts/gitAPI.js
@@ -33,9 +33,53 @@ class GitAPI extends API {
 
 	CountAllCommits(contributors_url, callback) {
 		this.callCallback(contributors_url, (data) => {
+			// TODO:
+
 			callback(data);
 		});
 	}
 }
 
 module.exports = GitAPI;
+
+// countAllCommits(refresh, callback) {
+// 	if (!this.totalCommitsInForks || refresh) {
+// 		const gitAPI = new GitAPI();
+// 		// const gitAPI = new GitAPI();
+// 		let fetched = 0;
+// 		this.getAllForks(refresh, () => {
+// 			// Get contributors per fork
+// 			this.forks.forEach((fork, i, forks) => {
+// 				gitAPI.callPromise(this.appData, fork.urls.contributors)
+// 					.then((data) => {
+// 						fetched++;
+// 						fork.contribData = data;
+// 						const ownerContributions = fork.contribData.filter((data) => {
+// 							return data.login === fork.owner;
+// 						});
+// 						fork.ownerContributions = ownerContributions[0];
+// 						if (fetched === forks.length) {
+// 							let count = this.forks.reduce((total, fork) => {
+// 								if (fork.ownerContributions === undefined) {
+// 									// Some people did fork, but didn't commit. Jerks
+// 									fork.ownerContributions = {
+// 										contributions: 0
+// 									};
+// 								}
+// 								return total + fork.ownerContributions.contributions;
+// 							}, 0);
+// 							this.totalCommitsInForks = count;
+// 							callback();
+// 						}
+// 					})
+// 					.catch((err) => {
+// 						debug.warn(err);
+// 						debug.warn('Repo: countAllCommits: callPromise: catch()');
+// 						return callback(false);
+// 					});
+// 			});
+// 		});
+// 	} else {
+// 		return callback();
+// 	}
+// }
